24d6e8accf48f2e48ef1ac8893066ed812a7f632
glotnidraminu <daniacolesnik@yandex.ru>
2026-04-06 16:18:06 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,7 @@
 const SPOTIFY_CONFIG = {
-    clientId: 'a44747bf12af49f7adbaef4070357d78',  
+    clientId: 'a44747bf12af49f7adbaef4070357d78',
     redirectUri: 'https://glotnidraminu.github.io/SME/',
-    scopes: [
-        'user-read-private',
-        'user-read-email',
-        'user-library-read',
-        'playlist-read-private',
-        'streaming'
-    ],
+    scopes: ['user-read-private', 'user-read-email'],
     authUrl: 'https://accounts.spotify.com/authorize',
     apiUrl: 'https://api.spotify.com/v1'
 };
